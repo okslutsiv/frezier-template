f0b845da619d56fed825fd220be3da2ed776223d
okslutsiv <okslutsiv@gmail.com>
2019-03-06 20:05:51 +0200

    404 fixed

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,25 @@
-import React from "react"
+import React from "react";
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+import withRoot from "../components/wrappers/MUIProvider/withRoot";
+import { withStyles } from "@material-ui/core/styles";
 
-const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
-)
+const styles = theme => ({
+  root: {
+    textAlign: "center",
+    paddingTop: "2rem",
+  },
+});
 
-export default NotFoundPage
+const NotFoundPage = props => {
+  return (
+    <Layout>
+      <SEO title="404: Not found" />
+      <h1>NOT FOUND</h1>
+      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    </Layout>
+  );
+};
+
+export default withRoot(withStyles(styles)(NotFoundPage));
